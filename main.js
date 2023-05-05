0c32a7c53c61cf607d11d0859277f7020dfc81dd
HilthEXE <endranomarklewence@gmail.com>
2023-05-06 00:26:55 +0800

    Finished the Shiny Ball

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+import { gsap } from "gsap";
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 // Scene
 const scene = new THREE.Scene();
@@ -6,9 +8,11 @@ const scene = new THREE.Scene();
 // * Create our sphere
 // Geometry is the shape or the 3D object
 const geometry = new THREE.SphereGeometry(3, 64, 32);
+
 // Material is the design of the shape
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  roughness: 0.2,
 });
 // mesh is the combination of the geometry and material
 const mesh = new THREE.Mesh(geometry, material);
@@ -23,6 +27,7 @@ const sizes = {
 // Light
 const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(0, 10, 10); // x, y, and z position
+light.intensity = 1.5;
 scene.add(light);
 
 // Camera
@@ -34,4 +39,67 @@ scene.add(camera);
 const canvas = document.querySelector(".webGL");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
 renderer.render(scene, camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+
+// Controls settings
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
+// Resize
+window.addEventListener("resize", () => {
+  // Update Sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update Camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+// Animation Loop
+const loop = () => {
+  controls.update(); // after adding Controls settings
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+
+loop(); // Continues re-rendering of the scene and camera
+
+// Timeline Magic
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
+tl.fromTo(".title", { opacity: 0, y: "-100%" }, { opacity: 1, y: "0%" });
+
+// Mouse Animation Color
+let mouseDown = false;
+let rgb = [];
+
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+
+    // Update the color
+    let newColor = new THREE.Color(`rgb(${rgb.join()})`);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
